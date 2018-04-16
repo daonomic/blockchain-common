67e68ba1111df47a183cca5e02aca0e0999cdab1
0v1se <dev@0v1se.com>
2018-04-16 22:59:27 +0300

    mono notifier

--- poller-mono/src/main/scala/io/daonomic/blockchain/poller/mono/MonoNotifier.scala
+++ poller-mono/src/main/scala/io/daonomic/blockchain/poller/mono/MonoNotifier.scala
@@ -2,10 +2,17 @@ package io.daonomic.blockchain.poller.mono
 
 import io.daonomic.blockchain.poller.Notifier
 import reactor.core.publisher.Mono
-
-import scala.collection.JavaConverters._
+import reactor.core.scheduler.Schedulers
 
 class MonoNotifier extends Notifier[Mono] {
+  private val scheduler = Schedulers.newSingle("mono-notifier")
+
   override def notify[T](list: Seq[T])(f: T => Mono[Unit]): Mono[Unit] =
-    MonoChain.consequently[T](list.asJava, value => f(value).then()).then(Mono.just())
+    if (list.isEmpty) {
+      Mono.just()
+    } else {
+      val head = list.head
+      val tail = list.tail
+      f(head).publishOn(scheduler).flatMap(_ => notify(tail)(f))
+    }
 }
